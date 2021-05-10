852ab67b8ec517a3b5fdbfaf2e72538e013d50f4
sohyun03 <sohun1103@naver.com>
2021-05-11 02:57:31 +0900

    css 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {HashRouter,Route} from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Navigation from "./components/Navigation";
+import "./App.css";
 
 //Route 안에는 두개의 props가 들어가는데 하나는 렌더링할 스크린이 들어가고 다른건 뭘 할지 정해줌
 //path 경로로 가고 가면  About 컴포넌트 보여줌
